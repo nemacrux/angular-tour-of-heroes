e3d00e3fdeb1ffb1132da72a30cd87a140e00329
nemacrux <nestor.mc@gmail.com>
2017-02-13 22:06:16 -0500

    Add configuration files. Add class Hero and template in app.component
    The template uses ngModel for two-way binding between Hero's name and
    input. ngModel was imported from FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// imported to use two-way binding
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  // external modules used by the app
+  imports:      [ 
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [ 
+    AppComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
