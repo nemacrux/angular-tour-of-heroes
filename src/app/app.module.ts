a426c343ddecb58625cd007ab9756f4e9c26fe62
nemacrux <nestor.mc@gmail.com>
2017-02-15 09:45:12 -0500

    Refactor AppComponent and HeroesComponent
    In order to separate the heroes view into a separate component
    because AppComponent should be a Dashboard now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
 // imported to use two-way binding
-import { FormsModule } from '@angular/forms';
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
 
 @NgModule({
   // external modules used by the app
-  imports:      [ 
+  imports: [ 
     BrowserModule,
     FormsModule
   ],
@@ -16,8 +19,11 @@ import { HeroDetailComponent } from './hero-detail.component';
   // that we created and that belong in our application's module.
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  // let angular to inject a new instance of HeroService
+  providers: [ HeroService ]
 })
 export class AppModule { }
